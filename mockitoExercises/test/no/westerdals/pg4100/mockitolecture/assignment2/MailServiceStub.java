cfecb0816e35a07582204dbbdbb2a37e319ca020
peremann <per.lauvaas@gmail.com>
2016-01-06 16:58:55 +0100

    testing2

---
+++ mockitoExercises/test/no/westerdals/pg4100/mockitolecture/assignment2/MailServiceStub.java
@@ -0,0 +1,17 @@
+package no.westerdals.pg4100.mockitolecture.assignment2;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+
+public class MailServiceStub implements MailService {
+
+	/**
+	 * Stub by placing the incoming message in a list of message (and do NOT
+	 * send anything). Also emplement a method for determining how many messages
+	 * have been sent (been placed in the list). Implementation needed for
+	 * exercise 2.
+	 */
+	public void send(Message msg) throws MessagingException {
+	}
+
+}
---
+++ mockitoExercises/test/no/westerdals/pg4100/mockitolecture/assignment3/ServletTest.java
@@ -0,0 +1,25 @@
+package no.westerdals.pg4100.mockitolecture.assignment3;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ServletTest {
+	
+	@Test
+	/**
+	 * Mocking.
+	 * Test:
+	 * <ul>
+	 * <li> the value of the request parameter "input" is used when calling model (helper)
+	 * </li>
+	 * <li> the servlet forwards the request
+	 * </li> 
+	 * </ul>
+	 * @throws Exception
+	 */
+	public void servletUsesModelAndForwardsRequest() throws Exception{
+		fail("Not implemented");
+	}
+
+}
